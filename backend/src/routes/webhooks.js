953e7aec78875cb55059e6100489507f2deaf0d9
Divya Sri <119841245+divyasrisaipravallika@users.noreply.github.com>
2026-01-11 15:42:04 +0530

    Delete backend directory

--- backend/src/routes/webhooks.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/payment", (req, res) => {
-  console.log("WEBHOOK RECEIVED:", req.body);
-  res.json({ received: true });
-});
-
-module.exports = router;
